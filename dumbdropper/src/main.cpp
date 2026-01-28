f344598384b7d59a805ce5f712c133cca6d99611
vassia38 <alex.19vasile@gmail.com>
2026-01-28 23:05:50 +0200

    don't forget to clean src's

---
+++ dumbdropper/src/main.cpp
@@ -0,0 +1,17 @@
+/****************
+* Helloworld.c
+* The most simplistic C program ever written.
+* An epileptic monkey on crack could write this code.
+*****************/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string s = "Hell0 world now my C++ program compiles";
+	cout << s << endl; 
+	return 0;
+}
+
